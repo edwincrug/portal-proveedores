d7435db34db285b53f791ee7d37bafe359f8cb5c
edwincrug <edwincrug@gmail.com>
2016-05-04 17:36:15 -0500

    PDF send first

--- app/static/angularJS/pages/admin/fileUploadController.js
+++ app/static/angularJS/pages/admin/fileUploadController.js
@@ -5,9 +5,22 @@ app.controller('fileUploadController', function($scope, FileUploader, File) {
         queueLimit: 2,
         url: '/api/fileUpload/files/'
     });
-
+    $('#fileModal').on("hidden.bs.modal", function() {
+        console.log("cerrando")
+        $scope.uploader.clearQueue();
+        $scope.uploaderDone = false;
+    })
     $scope.uploader.onAfterAddingFile = function(item, filter, options) {
         item.formData = [File.order];
+        if(  $scope.uploader.queue.length>1){
+            console.log(item.file.type)
+          if(item.file.type == "application/pdf"){
+             var temp = $scope.uploader.queue[0];
+             $scope.uploader.queue[0] = $scope.uploader.queue[1];
+             $scope.uploader.queue[1] = temp;
+          }
+        }
+
     }
 
     $scope.uploader.onCompleteAll = function() {
@@ -15,8 +28,6 @@ app.controller('fileUploadController', function($scope, FileUploader, File) {
     }
     $scope.closeModal = function() {
         $('#fileModal').modal('hide')
-        $scope.uploader.clearQueue();
-        $scope.uploaderDone = false;
     }
 
 })
