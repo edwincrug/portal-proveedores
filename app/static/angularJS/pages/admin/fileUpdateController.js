aac3eabde07f65dd0c85886ff2551234d15e789d
Osca Paredes <oparedes@bism.com.mx>
2016-06-03 13:59:49 -0500

    Image default

--- app/static/angularJS/pages/admin/fileUpdateController.js
+++ app/static/angularJS/pages/admin/fileUpdateController.js
@@ -5,7 +5,6 @@ app.controller('fileUpdateController', function($scope, File, Utils, Order) {
         $scope.idEstatus = File.order.idEstatus;
         $("#fileModalUpdateLabel").text("Orden  " + File.order.folio)
         Order.getDocuments(File.order.folio).then(function(d) {
-            console.log(d.data[0])
             var pdf = URL.createObjectURL(Utils.b64toBlob(d.data[0].arrayB, "application/pdf"))
             $("<object class='filesUpdate' data='" + pdf + "' width='100%' height='400px' >").appendTo('#pdfUpdateContent');
             if (d.data[0].pathXML != null)
