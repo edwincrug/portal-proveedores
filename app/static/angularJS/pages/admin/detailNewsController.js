08904aa460a2d3afd07ac796d9b42b4656869225
Osca Paredes <oparedes@bism.com.mx>
2016-06-07 18:30:22 -0500

    News detail for admins

--- app/static/angularJS/pages/admin/detailNewsController.js
+++ app/static/angularJS/pages/admin/detailNewsController.js
@@ -1,10 +1,10 @@
 app.controller('detailNewsController', function($scope, $stateParams, New, User) {
     User.me().then(function(user) {
-        New.getNews(user.data.rfc).then(function(data) {
-            for(var i in data.data){
-              if(data.data[i].idNoticia == $stateParams.id){
-                $scope.new = data.data[i];
-              }
+        New.getNews(user.ppro_idUserRol == 1 ? user.rfc : user.ppro_userId).then(function(data) {
+            for (var i in data.data) {
+                if (data.data[i].idNoticia == $stateParams.id) {
+                    $scope.new = data.data[i];
+                }
             }
         })
     })
